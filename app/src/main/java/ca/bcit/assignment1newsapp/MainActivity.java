f317ed1c40793a0193e9a426727496d0c2e96b20
Sehyung (Eric) Kim <kimeric97@hotmail.com>
2020-10-11 17:00:10 -0700

    MainActivity finished coding

--- app/src/main/java/ca/bcit/assignment1newsapp/MainActivity.java
+++ app/src/main/java/ca/bcit/assignment1newsapp/MainActivity.java
@@ -1,24 +1,42 @@
 package ca.bcit.assignment1newsapp;
 
-package com.example.kimer.goodfoodsmarket;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public final static String KEYWORD = "ca.bcit.assignment1newsapp_keyword";
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ListView list_continents = findViewById( R.id.list_categories );
-        list_continents.setOnItemClickListener( ( adapterView, view, position, id ) -> {
-            Intent i = new Intent( MainActivity.this, ProductTypeActivity.class );
-            i.putExtra( "index", (int) id );
-            startActivity( i );
-        } );
+        Button button = findViewById(R.id.search_button);
+        button.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view){
+        Button search_button = findViewById(R.id.search_button);
+        EditText search_bar = findViewById(R.id.search_bar);
+        String keyword = search_bar.getText().toString();
+
+        if ((view == search_button && !keyword.isEmpty())){
+            Intent i = new Intent(this, ResultsActivity.class);
+            i.putExtra(KEYWORD, keyword);
+            startActivity(i);
+        } else {
+            Toast.makeText(this, "Please enter keyword", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/ca/bcit/assignment1newsapp/ResultsActivity.java
@@ -0,0 +1,4 @@
+package ca.bcit.assignment1newsapp;
+
+public class ResultsActivity {
+}
